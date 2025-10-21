216109294f53b2997fcfd2c06c5169133062dadc
kimeunseokk <rpqhfls2whdk@gmail.com>
2025-10-21 17:41:52 +0900

    feat: 사용자 입력 처리 기능(자동차이름 및 시도횟수)

--- src/main/java/racingcar/Application.java
+++ src/main/java/racingcar/Application.java
@@ -16,13 +16,13 @@ public class Application {
     public static List<String> getCarName(){
         System.out.println("경주할 자동차 이름을 입력하세요(이름은 쉼표(,) 기준으로 구분)");
         String car = Console.readLine();
-        if(car == null) throw new IllegalArgumentException("입력값이 없습니다.");
+        if(car == null || car.trim().isEmpty()) throw new IllegalArgumentException("입력값이 없습니다.");
         return Arrays.asList(car.split(","));
     }
     public static int getValue(){
         System.out.println("시도할 횟수는 몇 회인가요?");
         String value = Console.readLine();
-        if(value == null) throw new IllegalArgumentException("시행횟수가 입력되지않았습니다.");
+        if(value == null || value.trim().isEmpty()) throw new IllegalArgumentException("시행횟수가 입력되지않았습니다.");
         int number = Integer.parseInt(value);
         return number;
     }
