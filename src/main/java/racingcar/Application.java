d3af6fc49640978dd528c7a7178a3563a049498e
kimeunseokk <rpqhfls2whdk@gmail.com>
2025-10-23 12:48:55 +0900

    feat: 게임 진행상황 출력(우승자 출력 전)

--- src/main/java/racingcar/Application.java
+++ src/main/java/racingcar/Application.java
@@ -7,6 +7,7 @@ import java.util.List;
 import java.util.Map;
 import java.util.function.Function;
 import java.util.stream.Collectors;
+import java.util.stream.IntStream;
 
 import camp.nextstep.edu.missionutils.Console;
 
@@ -14,11 +15,13 @@ public class Application {
     public static void main(String[] args) {
         // TODO: 프로그램 구현
         LinkedHashMap<String,Integer> carNameMap = getCarNames();
-        for(String value : carNameMap.keySet()){
-            System.out.print(value);
-        } // --> List에 잘 들어갔는지 디버깅확인용 코드입니다.5
-        int num = getValue();
+        // for(String value : carNameMap.keySet()){
+        //     System.out.print(value);
+        // } // --> List에 잘 들어갔는지 디버깅확인용 코드입니다.5
+        int attemp = getValue();
 
+        gamestart(carNameMap, attemp);
+        
     }
     public static LinkedHashMap<String,Integer> getCarNames(){
         System.out.println("경주할 자동차 이름을 입력하세요(이름은 쉼표(,) 기준으로 구분)");
@@ -27,6 +30,7 @@ public class Application {
         return Arrays.stream(car.split(",")).map(String::trim).collect(Collectors.toMap(name -> name, name -> 0, (v1,v2) -> v1, LinkedHashMap::new));
         // stream의 과정 1. stream 생성 2. 가공 3. 결과
     }
+
     public static int getValue(){
         System.out.println("시도할 횟수는 몇 회인가요?");
         String value = Console.readLine();
@@ -34,10 +38,23 @@ public class Application {
         int number = Integer.parseInt(value);
         return number;
     }
+
     public static int getRandomNumber(){
         int RandNum = Randoms.pickNumberInRange(0,9);
-        if(RandNum>=4) return 1;
+        if(RandNum >= 4) return 1;
         else return -1;
     }
+
+    public static void gamestart(LinkedHashMap<String, Integer> carPoint, int getAttempt){
+        for(int i=0; i<getAttempt; i++){
+            for(String name : carPoint.keySet()){
+                 if(getRandomNumber()==1){ carPoint.put(name,carPoint.getOrDefault(name, 0)+1); }
+                 System.out.println(name+" : "+"-".repeat(carPoint.get(name)));
+            }
+            System.out.println();
+        }
+        
+    }
+    
     
 }
